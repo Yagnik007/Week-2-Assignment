29b209b40b0bb5abde1b0343ca48e6a7bef06902
Yagnik007 <yagniktalaviya4122002@gmail.com>
2024-01-12 19:25:10 +0530

    friday last commm reallity

--- Data-structures/Dynamic Programming/5.cpp
+++ Data-structures/Dynamic Programming/5.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int func(vector<vector<int>>&dp, string s, string t, int n, int m)
+int func(int i, int j, string s, string t, vector<vector<int>>&dp)
 {
-    if(n == 0 or m == 0)
-    {
-        
-    }
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i] == t[j]) return dp[i][j] = func(i-1,j-1,s,t,dp);
+        int de = func(i-1,j,s,t,dp);
+        int ins = func(i,j-1,s,t,dp);
+        int rep = func(i-1,j-1,s,t,dp);
+        return dp[i][j] = 1 + min(de,min(ins,rep));
 }
 
 int main()
@@ -16,6 +20,6 @@ int main()
     cin>>s;
     cout<<"Enter the second string: ";
     cin>>t;
-    vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1,-1));
-    cout<<"Distance needed to edit is: "<<func(dp,s,t,s.size(),t.size())<<endl;
+    vector<vector<int>> dp(s.size(), vector<int>(t.size(),-1));
+    cout<<"Distance needed to edit is: "<<func(s.size()-1,t.size()-1,s,t,dp)<<endl;
 }
